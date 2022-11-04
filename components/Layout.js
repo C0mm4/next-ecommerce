f029866726151503981795bc709c165d52695e4a
C0mm4 <comonhuman@gmail.com>
2022-11-04 10:08:30 +0900

    10-UserLogin

--- components/Layout.js
+++ components/Layout.js
@@ -1,28 +1,45 @@
-import Head from "next/head";
-import React, { useContext, useState, useEffect } from "react";
-import Link from "next/link";
-import { Store } from "../utils/Store";
+import Head from 'next/head';
+import Link from 'next/link';
+import { React, useContext, useEffect, useState } from 'react';
+import { Store } from '../utils/Store';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useSession } from 'next-auth/react';
+import { signOut } from 'next-auth/react';
+import { Menu } from '@headlessui/react';
+import DropdownLink from './DropdownLink';
+import Cookies from 'js-cookie';
 
 export default function Layout({ title, children }) {
-  const { state } = useContext(Store);
+  const { status, data: session } = useSession();
+  const { state, dispatch } = useContext(Store);
   const { cart } = state;
-  const [cartItemsCount, setCartItemsCount] = useState(0)
+  const [cartItemsCount, setCartItemsCount] = useState(0);
   useEffect(() => {
-    setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0))
-    }, [cart.cartItems])
-    
+    setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
+  }, [cart.cartItems]);
+
+  const logoutClickHandler = () => {
+    Cookies.remove('cart');
+    dispatch({ type: 'CART_RESET' });
+    signOut({ callbackUrl: '/login' });
+  };
+
   return (
-    <div>
+    <>
       <Head>
-        <title>{title ? title + " - NextShop" : "NextShop"}</title>
-        <meta name="description" content="Nextjs Ecommerce" />
+        <title>{title ? title + ' - NextShop' : 'NextShop'}</title>
+        <meta name="description" content="Nextjs" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex-min-h-screen flex-col justify-between">
+
+      <ToastContainer position="bottom-center" limit={1} />
+
+      <div className="flex min-h-screen flex-col justify-between">
         <header>
-          <nav className="flex h-12 items-center px-4 justify-between shadow-md bg-slate-200">
+          <nav className="flex flex-row h-12 items-center px-4 justify-between shadow-md bg-slate-200">
             <Link href="/">
-              <a className="text-lg font-bold"> Next Shop</a>
+              <a className="text-lg font-bold">NextShop</a>
             </Link>
             <div>
               <Link href="/profile">
@@ -38,17 +55,52 @@ export default function Layout({ title, children }) {
                   )}
                 </a>
               </Link>
-              <Link href = '/login'>
-                <a className = 'p-2'>Login</a>
-              </Link>
+
+              {status === 'loading' ? (
+                'Loading'
+              ) : session?.user ? (
+                <Menu as="div" className="relative inline-block">
+                  <Menu.Button className="text-blue-600">
+                    {session.user.name}
+                  </Menu.Button>
+                  <Menu.Items className="absolute right-0 w-56 origin-top-right bg-white  shadow-lg ">
+                    <Menu.Item>
+                      <DropdownLink className="dropdown-link" href="/profile">
+                        Profile
+                      </DropdownLink>
+                    </Menu.Item>
+                    <Menu.Item>
+                      <DropdownLink
+                        className="dropdown-link"
+                        href="/order-history"
+                      >
+                        Order History
+                      </DropdownLink>
+                    </Menu.Item>
+                    <Menu.Item>
+                      <a
+                        className="dropdown-link"
+                        href="#"
+                        onClick={logoutClickHandler}
+                      >
+                        Logout
+                      </a>
+                    </Menu.Item>
+                  </Menu.Items>
+                </Menu>
+              ) : (
+                <Link href="/login">
+                  <a className="p-2">Login</a>
+                </Link>
+              )}
             </div>
           </nav>
         </header>
+        <main className="container m-auto mt-4">{children}</main>
+        <footer className="flex h-10 justify-center items-center shadow-inner bg-red-100">
+          <p>Copyright &copy; 2022 Nextshop</p>
+        </footer>
       </div>
-      <main className="container m-auto mt-4 px-4">{children}</main>
-      <footer className="flex h-10 justify-center items-center shadow-inner bg-red-100">
-        <p>Copyright &copy; 2022 NextShop</p>
-      </footer>
-    </div>
+    </>
   );
 }
